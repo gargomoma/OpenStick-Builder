957b6356c02abdbc22c45d9d4049b97a49bd638c
gargomoma <gargomoma@users.noreply.github.com>
2026-02-01 23:16:27 +0100

    Update debootstrap.sh change apk address

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -82,7 +82,7 @@ chmod 0600 ${CHROOT}/etc/NetworkManager/system-connections/*
 cp configs/99-custom.conf ${CHROOT}/etc/NetworkManager/conf.d/
 
 # install kernel
-wget -O - http://mirror.postmarketos.org/postmarketos/master/aarch64/linux-postmarketos-qcom-msm8916-6.12.1-r2.apk \
+wget -O - http://mirror.postmarketos.org/postmarketos/master/aarch64/linux-postmarketos-qcom-msm8916-6.12.1-r3.apk \
     | tar xkzf - -C ${CHROOT} --exclude=.PKGINFO --exclude=.SIGN* 2>/dev/null
 
 mkdir -p ${CHROOT}/boot/extlinux
